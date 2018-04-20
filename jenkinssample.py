63341358dc040f33df702a6e61f24481b872dbce
Albert Sison <asison89@gmail.com>
2018-04-19 20:46:09 -0700

    comment out SC tunnel desired cap

--- jenkinssample.py
+++ jenkinssample.py
@@ -13,8 +13,8 @@ desired_cap = {
     'platform': os.environ.get('SELENIUM_PLATFORM'),
     'browserName': os.environ.get('SELENIUM_BROWSER'),
     'version': os.environ.get('SELENIUM_VERSION'),
-    'name': "Jenkins Sample Test",
-    'tunnelIdentifier': os.environ.get('TUNNEL_IDENTIFIER')
+#     'name': "Jenkins Sample Test",
+#     'tunnelIdentifier': os.environ.get('TUNNEL_IDENTIFIER')
 #     'tunnelIdentifier': 'tunnel1'
 }
 
