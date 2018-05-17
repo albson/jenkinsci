5f58d5e7e328cddd9d5397581a875c4320134050
Albert Sison <asison89@gmail.com>
2018-05-17 10:20:32 -0700

    change build name

--- jenkinssample.py
+++ jenkinssample.py
@@ -16,7 +16,8 @@ desired_cap = {
     'browserName': os.environ.get('SELENIUM_BROWSER'),
     'version': os.environ.get('SELENIUM_VERSION'),
     'name': test_name,
-    'build': os.environ.get('JENKINS_BUILD_NUMBER')
+    'build': "testbuilder01"
+#     'build': os.environ.get('JENKINS_BUILD_NUMBER')
 #     'name': "Jenkins Sample Test",
 #     'tunnelIdentifier': os.environ.get('TUNNEL_IDENTIFIER')
 #     'tunnelIdentifier': 'tunnel1'
